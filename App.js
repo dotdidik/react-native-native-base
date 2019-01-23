fe9b17a64c083ccca640f13be81d94d72113f3a7
dotdidik <didik.irawann@gmail.com>
2019-01-23 20:15:10 +0700

    didik menambahkan conditional untuk loading

--- App.js
+++ App.js
@@ -1,25 +1,71 @@
 import React, {Component} from 'react';
 import {StyleSheet} from 'react-native';
 
-import { Container, Header, Left, Body, Right, Icon, Button, Text, Content, List, ListItem, Thumbnail } from 'native-base';
+import { Container, 
+  Header, 
+  Left, 
+  Body, 
+  Right, 
+  Icon, 
+  Button, 
+  Text, 
+  Content, 
+  List, 
+  ListItem, 
+  Thumbnail,
+  Spinner
+ } from 'native-base';
 import { Actions } from 'react-native-router-flux';
+import { Bubbles, DoubleBounce, Bars, Pulse } from 'react-native-loader';
 import axios from 'axios';
 
 export default class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-        posts: []
+        posts: [],
+        loading: true
     }
 }
 
 componentDidMount() {
     axios.get('https://learn-rest.herokuapp.com/api/products/')
     .then(res => {
-        this.setState({ posts:res.data })
+        this.setState({ posts:res.data,
+        loading: false })
     })
 }
   render() {
+    let contentload;
+    if(this.state.loading){
+      contentload = <Content contentContainerStyle={{ justifyContent: 'center', flex: 1, left:'45%' }}>
+                      <Bars size={15} color="blue" />
+                    </Content>
+    } else {
+      contentload = <Content>
+                      <List>
+                      {
+                        this.state.posts.slice(0,2).map((post, index) =>
+                          <ListItem onPress= {() => {Actions.detail({pd: post})}} key={index}>
+                            <ListItem avatar>
+                            </ListItem>
+                              <Left>
+                                <Thumbnail source={{ uri:  post.gambar }} />
+                              </Left>
+                              <Body>
+                                <Text>{post.nama}</Text>
+                                <Text note>{post.keterangan}</Text>
+                              </Body>
+                              <Right>
+                                <Text note>{post.harga}</Text>
+                              </Right>
+                          </ListItem>
+                        )
+                      }
+                      </List>
+                    </Content>
+    }
+
     return (
       <Container>
         <Header>
@@ -37,28 +83,7 @@ componentDidMount() {
             </Button>
           </Right>
         </Header>
-        <Content>
-          <List>
-          {
-            this.state.posts.slice(0,2).map((post, index) =>
-              <ListItem onPress= {() => {Actions.detail({pd: post})}} key={index}>
-                <ListItem avatar>
-                </ListItem>
-                  <Left>
-                    <Thumbnail source={{ uri:  post.gambar }} />
-                  </Left>
-                  <Body>
-                    <Text>{post.nama}</Text>
-                    <Text note>{post.keterangan}</Text>
-                  </Body>
-                  <Right>
-                    <Text note>{post.harga}</Text>
-                  </Right>
-              </ListItem>
-            )
-          }
-          </List>
-        </Content>
+        {contentload}
       </Container>
     );
   }
